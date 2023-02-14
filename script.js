f3fad0a9fecb94fed6b42a0d68d5a8160ce9155b
Kajetan <kajetan.loska@op.pl>
2023-02-14 14:29:47 +0100

    added hamburger animation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const mainNavList = document.querySelector('.main-nav__list');
 const hamburgerButton = document.querySelector('.main-nav__hamburger');
 hamburgerButton.addEventListener('click', () => {
+  hamburgerButton.classList.toggle('open');
   mainNavList.classList.toggle('open');
 });
